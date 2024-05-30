d428ee9e78039eb065be47bca8a0044892403d7d
alphabetseng <162334353+alphabetseng@users.noreply.github.com>
2024-05-30 12:04:05 +0300

    Add files via upload

---
+++ webcam_goruntu_alma.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow(mat=frame, winname="cap")
+    cap = cv2.VideoCapture(0)
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
