7a5778956f57fa1bc19b816b655e3cc6f1c71a18
zeyneb <162334353+ngrgulfemzynbgozalici@users.noreply.github.com>
2025-02-10 17:57:37 +0300

    Update circleAndRectangle.py

--- circleAndRectangle.py
+++ circleAndRectangle.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-backround = np.zeros((512, 512, 3), np.uint8) + 128 # 0 = black => 255 = white
+backround = np.zeros((512, 512, 3), np.uint8) + 128 # 0 = black , 255 = white
 
 rectangle = cv.rectangle(backround, (100, 100), (150, 155), (255, 200, 0), thickness=-1)
 circle = cv.circle(backround, (250, 250), 30, (0, 0, 255), thickness=-1)
