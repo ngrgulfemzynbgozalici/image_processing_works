517bbaa993c56cd27094841b45e647d7e83e8ae8
zeyneb <162334353+ngrgulfemzynbgozalici@users.noreply.github.com>
2025-03-05 13:29:34 +0300

    Update circleAndRectangle.py

--- circleAndRectangle.py
+++ circleAndRectangle.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 import numpy as np
 
-backround = np.zeros((512, 512, 3), np.uint8) + 128 # 0 = black , 255 = white
+background = np.zeros((512, 512, 3), np.uint8) + 128 # 0 = black , 255 = white
 
-rectangle = cv.rectangle(backround, (100, 100), (150, 155), (255, 200, 0), thickness=-1)
-circle = cv.circle(backround, (250, 250), 30, (0, 0, 255), thickness=-1)
+rectangle = cv.rectangle(background, (100, 100), (150, 155), (255, 200, 0), thickness=-1)
+circle = cv.circle(background, (250, 250), 30, (0, 0, 255), thickness=-1)
 
 cv.namedWindow("circle and rectangle", 0)
 
-cv.imshow("circle and rectangle", backround)
+cv.imshow("circle and rectangle", background)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
